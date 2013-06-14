ef501c7ea62f4448f640b56745e4c0b0156f01eb
Jawaad Mahmood <ideas@jawaadmahmood.com>
2013-06-14 16:02:00 +0900

    Update server.py
    Adding dynamic port assignment

--- example/server/server.py
+++ example/server/server.py
@@ -6,12 +6,14 @@ import tornado.web
 import tornado.ioloop
 import tornado.options
 
+define("port", default=1690, help="run on the given port", type=int)
 tornado.options.parse_config_file("test_server.conf")
 tornado.options.parse_command_line()
 application = tornado.web.Application([
     (r'/tests', unittestWebSocketHandler),
 ])
 
+logging.info("starting Tornado web server on port %d" % tornado.options.options['port'].value())
 http_server = tornado.httpserver.HTTPServer(application)
-http_server.listen(1690)
+http_server.listen(int(tornado.options.options['port'].value()))
 tornado.ioloop.IOLoop.instance().start()
