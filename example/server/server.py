ac3bff118f0c05db944742a20fb21ca171780f63
Jawaad Bin Mahmood <jawaad@MathBook Pro.(none)>
2013-06-18 21:22:00 +0900

    Fixed a few bugs so example server runs out of the box.

--- example/server/server.py
+++ example/server/server.py
@@ -6,14 +6,16 @@ import tornado.web
 import tornado.ioloop
 import tornado.options
 
-define("port", default=1690, help="run on the given port", type=int)
+tornado.options.define("port", default=1690, help="run on the given port", type=int)
 tornado.options.parse_config_file("test_server.conf")
 tornado.options.parse_command_line()
 application = tornado.web.Application([
     (r'/tests', unittestWebSocketHandler),
 ])
 
-logging.info("starting Tornado web server on port %d" % tornado.options.options['port'].value())
+logging.info("starting Tornado web server on port %d" % tornado.options.options['port'])
 http_server = tornado.httpserver.HTTPServer(application)
-http_server.listen(int(tornado.options.options['port'].value()))
+
+port = tornado.options.options['port']
+http_server.listen(int(port))
 tornado.ioloop.IOLoop.instance().start()
